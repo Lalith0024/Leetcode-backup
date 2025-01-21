f74db877da73309214f6a09289aff089c6817456
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-01-21 14:08:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (100.00%)

--- 0075-sort-colors/solution.py
+++ 0075-sort-colors/solution.py
@@ -1,8 +1,17 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+       
         nums.sort()
         return nums
+            
+
+
+            
+            
+
+
+        
+        
+
+
         
