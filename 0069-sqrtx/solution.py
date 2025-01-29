3382f26e0bd2ef1b88a7987575b23608a60b0116
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-01-29 08:34:26 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18 MB (100.00%)

--- 0069-sqrtx/solution.py
+++ 0069-sqrtx/solution.py
@@ -1,4 +1,14 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(x**0.5)
+        left,right=1,x
+        while left<=right:
+            mid=(left+right)//2
+            if mid*mid==x:
+                return mid
+            if mid*mid>x:
+                right=mid-1
+            else:
+                left=mid+1
+        return right
+
         
