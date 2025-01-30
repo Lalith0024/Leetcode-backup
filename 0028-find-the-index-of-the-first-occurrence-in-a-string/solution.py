78adfe1f9ab3121e5cc7bac133cc8d841c96a15f
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-01-30 08:21:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.8 MB (100.00%)

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        m, n = len(haystack), len(needle)
+        for i in range(m - n + 1):
+            if haystack[i:i + n] == needle: #slicing easy part juz check this out
+                return i
+        return -1
+        
+        
+        
