e17bf841f153028d3819937ff999a9035fcb6773
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-02-03 04:07:11 +0000

    Sync LeetCode submission Runtime - 33 ms (97.90%), Memory - 17.8 MB (100.00%)

---
+++ 0278-first-bad-version/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        first, last = 1, n
+
+        while first < last:
+            mid = first + (last - first) // 2
+
+            if isBadVersion(mid):
+                last = mid  # The first bad version could be mid or before.
+            else:
+                first = mid + 1  # The first bad version must be after mid.
+
+        return first
+
+        
