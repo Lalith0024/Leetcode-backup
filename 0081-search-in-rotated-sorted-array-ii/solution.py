874e091bc7765a99c5ab3ea2804ea464e55cb327
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-02-19 14:00:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.9 MB (5.67%)

---
+++ 0081-search-in-rotated-sorted-array-ii/solution.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def search(self, nums, target):
+        l, h = 0, len(nums) - 1
+
+        while l <= h:
+            mid = (l + h) // 2
+
+            if nums[mid] == target:
+                return True
+
+            if nums[l] == nums[mid] == nums[h]:
+                l += 1
+                h -= 1
+                continue
+
+            if nums[l] <= nums[mid]:  
+                if nums[l] <= target < nums[mid]:
+                    h = mid - 1
+                else:
+                    l = mid + 1
+            else:
+                if nums[mid] < target <= nums[h]:
+                    l = mid + 1
+                else:
+                    h = mid - 1
+
+        return False
