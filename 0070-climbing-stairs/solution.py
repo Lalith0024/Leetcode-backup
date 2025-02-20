18e841c1364b7a5defe7e17ea4fbd9d3305483fb
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-02-20 04:11:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.5 MB (19.78%)

--- 0070-climbing-stairs/solution.py
+++ 0070-climbing-stairs/solution.py
@@ -1,16 +1,12 @@
 class Solution(object):
     def climbStairs(self, n):
-        if n <= 3: return n
-
-        prev1 = 3
-        prev2 = 2
-        cur = 0
-
-        for _ in range(3, n):
-            cur = prev1 + prev2
-            prev2 = prev1
-            prev1 = cur
         
-        return cur
-
-        
+        if n<=1:
+            return 1
+        dp = [0]*(n+1)
+        dp[0]=1
+        dp[1]=1
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
+            
