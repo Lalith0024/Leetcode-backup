c604bf9254b2cbc8c83ea8014cd5ac633be85ca6
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-02-22 15:23:40 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.5 MB (17.82%)

--- 0075-sort-colors/solution.py
+++ 0075-sort-colors/solution.py
@@ -1,17 +1,25 @@
-class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-       
-        nums.sort()
-        return nums
-            
+class Solution(object):
+    def sortColors(self, nums):
+        high = len(nums)-1
+        low = 0
+        mid  = 0
+        while mid<=high:
+            if nums[mid]==0:
+                nums[mid],nums[low]=nums[low],nums[mid]
+                low+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[high]=nums[high],nums[mid]
+                
+                high-=1
 
-
-            
-            
-
+        return nums #popular dutch method
 
         
-        
+
+
 
 
         
