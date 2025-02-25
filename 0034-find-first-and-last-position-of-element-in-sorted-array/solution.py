fb506ed462481316cd3d5c1b5db9d2edbc953075
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-02-25 17:38:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.2 MB (38.57%)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
@@ -1,18 +1,34 @@
-class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if target not in nums:
-            return -1,-1
-        lst = []
-        for i in range(len(nums)):
-            if nums[i]==target:
-                lst.append(i)
-                break
-        last = 0
-        for i in range(len(nums)):
-            if nums[i]==target:
-                last = i
-        lst.append(last)
+class Solution(object):
+    def searchRange(self, nums, target):
+        ans = [-1,-1]
+        l  = 0
+        h = len(nums)-1
+        res=-1
+        while l<=h:
+            mid = (l+h)//2
+            if nums[mid]==target:
+                res = mid
+                h = mid-1
+            elif nums[mid]>target:
+                h = mid-1
+            else:
+                l = mid+1
+        ans[0]=res
+        res2 = -1
+        l  = 0
+        h = len(nums)-1
+        while l<=h:
+            mid = (l+h)//2
+            if nums[mid]==target:
+                res2 = mid
+                l = mid+1
+            elif nums[mid]>target:
+                h = mid-1
+            else:
+                l = mid+1
+        ans[1]=res2
+        return ans
+                
         
 
-        return lst
         
