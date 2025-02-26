ad2ff4cbb3318ba94c62389ce412bbec245af4f1
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-02-26 15:54:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.5 MB (0.32%)

---
+++ 0953-reverse-only-letters/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def reverseOnlyLetters(self, s):
+        s = list(s)
+
+        left, right = 0, len(s)-1
+        while left < right:
+            if s[left].isalpha() and s[right].isalpha():
+
+                s[left], s[right] = s[right], s[left]
+                left += 1
+                right -= 1
+            elif s[left].isalpha() and not s[right].isalpha():
+                right -=1
+            else:
+                left+=1
+
+
+        return ''.join(s)
