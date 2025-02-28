55ffcd89b34aa921d1baa1fc1d1b459181289872
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-02-28 00:22:53 +0000

    Sync LeetCode submission Runtime - 3 ms (99.87%), Memory - 12.4 MB (16.33%)

--- 1013-fibonacci-number/solution.py
+++ 1013-fibonacci-number/solution.py
@@ -1,10 +1,13 @@
-class Solution:
-    def fib(self, n: int) -> int:
-        def fibb(n):
-            if n==0:
-                return 0
-            if n==1:
-                return 1
-            return fibb(n-1)+fibb(n-2)
-        return fibb(n)
-            
+class Solution(object):
+    def fib(self, n):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        Fibonacci = [0] * (n + 1)
+        Fibonacci[0] = 0
+        Fibonacci[1] = 1
+        for i in range(2, n + 1):
+            Fibonacci[i] = Fibonacci[i-1] + Fibonacci[i-2]
+        return Fibonacci[n]
+
