01f852402b22d60fb164050eb4998b8d1a131755
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-02-28 01:01:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.3 MB (35.56%)

--- 0792-binary-search/solution.py
+++ 0792-binary-search/solution.py
@@ -1,14 +1,6 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        start = 0
-        end= len(nums)-1
-        while start<=end:
-            mid = (start+end)//2
-            if nums[mid]>target:
-                end = mid-1
-            elif nums[mid]<target:
-                start = mid+1
-            else:
-                return mid
+class Solution(object):
+    def search(self, nums, target):
+        for i in range(len(nums)):
+            if nums[i]==target:
+                return i
         return -1
-        
