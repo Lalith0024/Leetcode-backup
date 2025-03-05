51e607f57720e5dc23f3235b7cd33460bd4d2388
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-03-05 16:59:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19 MB (100.00%)

--- 0001-two-sum/solution.py
+++ 0001-two-sum/solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        ans = []
-        for i in range(len(nums)):
-            for j in range(i):
-                if nums[i]+nums[j]==target:
-                    ans.append(i)
-                    ans.append(j)
-        return ans
+        prevMap = {} 
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+
         
