bf2934f27a257d19158a0e14152521a222421fef
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-04-24 04:07:39 +0000

    Sync LeetCode submission Runtime - 4 ms (7.71%), Memory - 18 MB (100.00%)

---
+++ 0083-remove-duplicates-from-sorted-list/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if not head:
+            return None
+        
+        temp = head
+        temp2 = head.next
+        last = head.val
+        
+        while temp2:  # while end of Linked list
+            if temp2.val == last:  # Current number same as last number
+                if not temp2.next:  # If last element, just delete and break loop
+                    temp.next = None
+                    break
+                temp2 = temp2.next  # Not last, then delete that element
+                temp.next = temp2  # and move to next element
+            else:  # If not the same as last element, jump to next node
+                temp = temp2
+                last = temp.val
+                temp2 = temp2.next
+        
+        return head  # return the head back
