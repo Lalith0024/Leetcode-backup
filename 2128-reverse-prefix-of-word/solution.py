b8205100acb00a350c352915aa0fca0dd8c363da
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-05-30 05:00:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.6 MB (100.00%)

--- 2128-reverse-prefix-of-word/solution.py
+++ 2128-reverse-prefix-of-word/solution.py
@@ -1,8 +1,12 @@
-class Solution(object):
-    def reversePrefix(self, word, ch):
-        index = word.find(ch) 
-        if index != -1: 
-            return word[:index+1][::-1] + word[index+1:] 
-        return word
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        ans = ""
+        l = 0
+        while l < len(word) and word[l] != ch:
+            l += 1
+        if l == len(word):
+            return word  # character not found
+        ans += word[:l+1][::-1]
+        ans += word[l+1:]
+        return ans
 
-        
