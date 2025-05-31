2445f30ffc7ccdaec117c5bd85acde1932ec7ae8
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-05-31 16:22:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.1 MB (100.00%)

---
+++ 1677-matrix-diagonal-sum/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        s = 0
+        n = len(mat)
+        
+        for i in range(n):
+            s += mat[i][i]            
+            s += mat[i][n - 1 - i]     
+        
+        
+        if n % 2 == 1:
+            s -= mat[n // 2][n // 2]
+        
+        return s
+
