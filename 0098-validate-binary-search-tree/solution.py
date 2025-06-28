1aedcb00b278afa31d39053d1ddf20a393c3e75d
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-06-28 06:43:21 +0000

    Sync LeetCode submission Runtime - 4 ms (38.96%), Memory - 17.5 MB (6.09%)

--- 0098-validate-binary-search-tree/solution.py
+++ 0098-validate-binary-search-tree/solution.py
@@ -1,15 +1,33 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def bst(root, min_val=float('-inf'), max_val=float('inf')):
-            if root is None:
-                return True
-            if not (min_val < root.val < max_val):
-                return False
-            return bst(root.left, min_val, root.val) and bst(root.right, root.val, max_val)
-        return bst(root)
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        output =[]
+        self.inorder(root, output)
+        
+        for i in range(1, len(output)):
+			if output[i-1]>= output[i]:
+				return False
+        
+        return True
+    
+    # Time complexity of inorder traversal is O(n)
+    # Fun fact: Inorder traversal leads to a sorted array if it is 
+    # a Valid Binary Search. Tree.
+    def inorder(self, root, output):
+        if root is None:
+            return
+        
+        self.inorder(root.left, output)
+        output.append(root.val)
+        self.inorder(root.right, output)
+        
