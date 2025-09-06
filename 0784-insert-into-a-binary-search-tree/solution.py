9a5f36d59d981c3124b1a55999363540892e8d41
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-09-06 09:31:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (100.00%)

---
+++ 0784-insert-into-a-binary-search-tree/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:                      # handle empty tree
+            return TreeNode(val)
+        
+        curr = root
+        while True:
+            if curr.val > val:
+                if curr.left:
+                    curr = curr.left
+                else:
+                    curr.left = TreeNode(val)
+                    break
+            else:
+                if curr.right:
+                    curr = curr.right
+                else:
+                    curr.right = TreeNode(val)
+                    break
+        return root
+
