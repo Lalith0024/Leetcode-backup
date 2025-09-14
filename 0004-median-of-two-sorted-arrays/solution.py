8dfe52bce89c1f35211819b6583b9311108be743
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2025-09-14 16:09:42 +0000

    Sync LeetCode submission Runtime - 4 ms (28.77%), Memory - 18 MB (100.00%)

--- 0004-median-of-two-sorted-arrays/solution.py
+++ 0004-median-of-two-sorted-arrays/solution.py
@@ -1,36 +1,19 @@
-class Solution(object):
+class Solution:
     def findMedianSortedArrays(self, nums1, nums2):
+        # Merge the arrays into a single sorted array.
+        merged = nums1 + nums2
 
-        n1 = len(nums1)
-        n2 = len(nums2)
+        # Sort the merged array.
+        merged.sort()
 
-        # If nums1 is larger than nums2, swap them to ensure n1 is smaller than n2.
-        if n1 > n2:
-            return self.findMedianSortedArrays(nums2, nums1)
+        # Calculate the total number of elements in the merged array.
+        total = len(merged)
 
-        l = 0
-        r = n1
-        while l <= r:
-            mid1 = (l + r) / 2
-            mid2 = (n1 + n2 + 1) / 2 - mid1
-
-            maxLeft1 = nums1[mid1-1] if mid1 != 0 else float('-inf')
-            minRight1 = nums1[mid1] if mid1 != n1 else float('inf')
-
-            maxLeft2 = nums2[mid2-1] if mid2 != 0 else float('-inf')
-            minRight2 = nums2[mid2] if mid2 != n2 else float('inf')
-
-            if maxLeft1 <= minRight2 and maxLeft2 <= minRight1:
-                if (n1 + n2) % 2 == 0:
-                    return float(max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2
-                else:
-                    return float(max(maxLeft1, maxLeft2))
-            elif maxLeft1 > minRight2:
-                r = mid1 - 1
-            else:
-                l = mid1 + 1
-
-        return -1
-        
-
-        
+        if total % 2 == 1:
+            # If the total number of elements is odd, return the middle element as the median.
+            return float(merged[total // 2])
+        else:
+            # If the total number of elements is even, calculate the average of the two middle elements as the median.
+            middle1 = merged[total // 2 - 1]
+            middle2 = merged[total // 2]
+            return (float(middle1) + float(middle2)) / 2.0
