d3b5e784911c535dc1b210f9a29f7fa2be1bc7ee
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2026-01-21 08:44:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (18.16%)

--- 0066-plus-one/solution.py
+++ 0066-plus-one/solution.py
@@ -1,11 +1,14 @@
-class Solution(object):
-    def plusOne(self, digits):
-        for i in range(len(digits)-1,-1,-1):
-            if digits[i]==9:
-                digits[i]=0
-            else:
-                digits[i]=digits[i]+1
-                return digits
-        return [1]+digits        
-        
-        
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        # so array length can be 1-100 okay 
+        # so basically focus on untis pace 9 -> 10
+        # what if  0 is in units place? 
+        string = ""
+        for i in range(len(digits)):
+            string += str(digits[i])
+        ans = int(string) + 1
+        converted_str = str(ans)
+        arr = []
+        for i in converted_str:
+            arr.append(int(i))
+        return arr
