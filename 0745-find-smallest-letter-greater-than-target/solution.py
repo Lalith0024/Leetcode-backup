91bd2ad886ef9d19b191c5d9bbb629db262f6899
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2026-02-03 07:02:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.8 MB (16.03%)

--- 0745-find-smallest-letter-greater-than-target/solution.py
+++ 0745-find-smallest-letter-greater-than-target/solution.py
@@ -1,10 +1,16 @@
-class Solution(object):
-    
-    def nextGreatestLetter(self, letters, target):
-        ans = letters[0]
-        for i in range(len(letters)):
-            if letters[i]>target:
-                ans = letters[i]
-                break
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        l = 0
+        r = len(letters) - 1
+        ans = letters[0]  # handles wraparound case
+
+        while l <= r:
+            mid = (l + r) // 2
+            if letters[mid] > target:
+                ans = letters[mid]
+                r = mid - 1
+            else:
+                l = mid + 1
+
         return ans
-        
+
