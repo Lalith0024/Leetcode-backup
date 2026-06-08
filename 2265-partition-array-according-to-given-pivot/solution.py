07bc46b84e0d4b1029ef2dcbc1bde65d82936075
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2026-06-08 17:33:35 +0000

    Sync LeetCode submission Runtime - 43 ms (26.34%), Memory - 34.4 MB (5.25%)

---
+++ 2265-partition-array-according-to-given-pivot/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        return (
+            [n for n in nums if n < pivot] +
+            [n for n in nums if n == pivot] +
+            [n for n in nums if n > pivot]
+        )
