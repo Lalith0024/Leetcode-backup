6847ca8b09fd884405f25494f905e7553e962cd7
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2026-06-25 04:22:39 +0000

    Sync LeetCode submission Runtime - 1854 ms (33.46%), Memory - 19.6 MB (5.86%)

---
+++ 4074-count-subarrays-with-majority-element-i/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countMajoritySubarrays(self, nums: List[int], target: int) -> int:
+        count = 0
+        for i in range(len(nums)):
+            freq = 0
+            for j in range(i, len(nums)):
+                if nums[j] == target:
+                    freq += 1
+                length = j - i + 1
+                if freq > length // 2:
+                    count += 1
+
+        return count
+
