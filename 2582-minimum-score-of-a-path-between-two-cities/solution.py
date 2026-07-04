a48b09c6633131b76e8e6888c8668ed5a5241480
Kasula Lalithendra <kasula.lalithendra2024@nst.rishihood.edu.in>
2026-07-04 13:53:34 +0000

    Sync LeetCode submission Runtime - 151 ms (55.89%), Memory - 102.6 MB (5.38%)

---
+++ 2582-minimum-score-of-a-path-between-two-cities/solution.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+class Solution:
+    def minScore(self, n, roads):
+        graph = defaultdict(list)
+
+        for a, b, d in roads:
+            graph[a].append((b, d))
+            graph[b].append((a, d))
+
+        ans = float('inf')
+        seen = set()
+
+        def dfs(node):
+            nonlocal ans
+
+            seen.add(node)
+
+            for nei, d in graph[node]:
+                ans = min(ans, d)
+
+                if nei not in seen:
+                    dfs(nei)
+
+        dfs(1)
+        return ans
